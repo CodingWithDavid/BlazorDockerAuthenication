f0eaa1bf6db9ea61723adf50e042924f58bcf3e4
David Gallivan <david@cosingwithdavid.com>
2022-04-29 21:27:33 -0700

    Initial file load

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,65 @@
+using BlazorApp1.Areas.Identity;
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+string rootpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
+rootpath = $"{rootpath}\\data\\UserMaintenance.mdf";
+string connStr = connectionString.Replace("%%DBNAME%%", rootpath);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connStr));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,51 @@
+# Project to user docker to deploy a Blazor server application with a local database
+
+This was to test out an idea I had to try to provide cheap and easy authenication to my simple Blazor applications.
+
+*** PLEASE NOTE THIS REQUIRES .NET 6, Visual Studio 2022 and Docker ***
+
+by [David Gallivan](http://twitter.com/CodingwithDavid)
+
+
+## Why
+
+Just looking for a cheap replacement for all my authenications for my applications.
+
+## Getting Started
+
+1. Clone this repository
+
+   ```Command Line
+   git clone https://github.com/CodingWithDavid/BlazorDockerAuthenication
+   cd BlazorDockerAuthenication
+   ```
+
+1.	Open in Visual Studio 2022 or Visual Code
+a.	With Visual Code you will need to install the C# extensions
+2.	Press F5
+
+## What's in the App
+
+1. Blazor Server side with basic authenication
+2. Docker build and deployment
+
+
+## Worth Noting
+Even though this project did not work as I had hoped, it was an ecellent learning opportunity Images were used to instead of icons for a change of pace.
+
+
+## Problems or Suggestions
+
+[Open an issue here]( https://github.com/CodingWithDavid/BlazorDockerAuthenication/issues)
+
+## Thank You
+
+
+## Resources
+
+- [VS Code](https://code.visualstudio.com)
+- [Visual Studio]( https://visualstudio.microsoft.com/)
+- [Docker]( https://www.docker.com )
+
+
+
